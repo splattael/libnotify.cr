815a704d6ecef1e087b6140458a5fe80f5defdfc
Arthur Poulet <arthur.poulet@mailoo.org>
2016-10-18 17:20:28 +0100

    Little improvment on Notification specs

--- spec/notification_spec.cr
+++ spec/notification_spec.cr
@@ -14,10 +14,11 @@ describe Libnotify::Notification do
     Libnotify::Notification.new do |notify|
       notify.summary    = "hello"
       notify.body       = "world"
-      notify.timeout    = 1.5         # 1.5 (s), 1000 (ms), "2", nil, false
+      notify.timeout    = 1.5
+      notify.timeout    = 1500
       notify.urgency    = :critical   # :low, :normal, :critical
-      notify.append     = false       # default true - append onto existing notification
-      notify.transient  = true        # default false - keep the notifications around after display
+      notify.append     = false       # default true
+      notify.transient  = true        # default false
       notify.icon_path  = "/usr/share/icons/gnome/scalable/emblems/emblem-default.svg"
     end
 
