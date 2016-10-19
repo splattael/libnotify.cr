9730fc85cbf1f0b08626bc479e82e4ac0e9c8b9c
Arthur Poulet <arthur.poulet@mailoo.org>
2016-10-19 09:57:21 +0100

    Improve the Notification's update system
    - `update` renamed to `update!` and set to private
    - add `update` to modify the fields
    - `show` calls update! before display anything
    - add some documentation lines
    - apply `crystal tool format`

--- src/libnotify/notification.cr
+++ src/libnotify/notification.cr
@@ -17,7 +17,7 @@ class Libnotify::Notification
                  @app_name = "default")
     init_app!
     @notify = C.notification_new @summary.to_s, @body.to_s, @icon_path.to_s
-    update
+    update!
   end
 
   def initialize(@summary = nil, @body = nil, @icon_path = nil,
@@ -27,20 +27,25 @@ class Libnotify::Notification
     init_app!
     @notify = C.notification_new @summary.to_s, @body.to_s, @icon_path.to_s
     yield self
-    update
+    update!
   end
 
-  def show(gerror = nil)
-    C.notification_show @notify, gerror
-    self
+  # Update **all fields** of the notification (in arguments or via the block).
+  #
+  # It resets the fields to their default value / nil or use the arguments.
+  # Then, it yield a block where it is possible to modify all of them.
+  def update(@summary = nil, @body = nil, @icon_path = nil,
+             @timeout = -1, @category = nil, @urgency = Libnotify::C::Notifyurgency::NotifyUrgencyNormal,
+             @append = false, @transient = true,
+             @app_name = "default", &block)
+    yield self
+    update!
   end
 
-  def update
-    C.notification_update @notify, @summary.to_s, @body.to_s, @icon_path.to_s
-    C.notification_set_timeout @notify, @timeout
-    C.notification_set_urgency @notify, @urgency
-    C.notification_set_category @notify, @category.to_s
-    C.notification_set_app_name @notify, @app_name
+  # Display the notification on the screen with the last modification
+  def show
+    update!
+    C.notification_show @notify, nil
     self
   end
 
@@ -49,12 +54,12 @@ class Libnotify::Notification
   #   @notify
   # end
 
-  # set the timeout at `sec`, in seconds
+  # Set the timeout at `sec`, in seconds
   def timeout=(sec : Float::Primitive)
     timeout = (sec * 1000).round
   end
 
-  # set the urgency, based on the symbols `:critical`, `:normal`, `:low`
+  # Set the urgency, based on the symbols `:critical`, `:normal`, `:low`
   def urgency=(urgency : Symbol)
     urgency = case urgency
               when :low
@@ -66,6 +71,15 @@ class Libnotify::Notification
               else
                 raise "Invalid Notifyurgency"
               end
+  end
+
+  private def update!
+    C.notification_update @notify, @summary.to_s, @body.to_s, @icon_path.to_s
+    C.notification_set_timeout @notify, @timeout
+    C.notification_set_urgency @notify, @urgency
+    C.notification_set_category @notify, @category.to_s
+    C.notification_set_app_name @notify, @app_name
+    self
   end
 
   private def init_app!
