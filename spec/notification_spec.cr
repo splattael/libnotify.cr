8aea78c571372a5ec9d8e504acd23aaf05a80c67
Arthur Poulet <arthur.poulet@mailoo.org>
2016-10-19 09:57:21 +0100

    Improve the Notification's update system
    - `update` renamed to `update!` and set to private
    - add `update` to modify the fields
    - `show` calls update! before display anything
    - add some documentation lines
    - apply `crystal tool format`

--- spec/notification_spec.cr
+++ spec/notification_spec.cr
@@ -12,34 +12,29 @@ describe Libnotify::Notification do
     end.show
 
     Libnotify::Notification.new do |notify|
-      notify.summary    = "hello"
-      notify.body       = "world"
-      notify.timeout    = 1.5
-      notify.timeout    = 1500
-      notify.urgency    = :critical   # :low, :normal, :critical
-      notify.append     = false       # default true
-      notify.transient  = true        # default false
-      notify.icon_path  = "/usr/share/icons/gnome/scalable/emblems/emblem-default.svg"
+      notify.summary = "hello"
+      notify.body = "world"
+      notify.timeout = 1.5
+      notify.timeout = 1500
+      notify.urgency = :critical # :low, :normal, :critical
+      notify.append = false      # default true
+      notify.transient = true    # default false
+      notify.icon_path = "/usr/share/icons/gnome/scalable/emblems/emblem-default.svg"
     end
-
   end
 
   it "Init and update" do
     n = Libnotify::Notification.new "update 1" do |n|
-      n.body = "Should be displayed"
-    end
-    n.body = "Should not be displayed"
-    n.show
-    n.update # not displayed either
-
-    sleep 0.5
-
-    n = Libnotify::Notification.new "update 2" do |n|
       n.body = "Should not be displayed"
     end
     n.body = "Should be displayed"
-    n.update.show
+    n.show
 
-    n = Libnotify::Notification.new summary: "big", body: "body text"
+    n = Libnotify::Notification.new "update 2 not displayed" do |n|
+      n.body = "Should not be displayed"
+    end
+    n.update(body: "Should be displayed") do |n|
+      n.summary = "update 2"
+    end.show
   end
 end
